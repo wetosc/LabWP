ff282b66ba033ce890afacb31d971a49a001c982
Cernei Eugenius <eugen.cernei@gmail.com>
2016-04-02 13:40:50 +0300

    Finished lab7

--- LAB7/main.cpp
+++ LAB7/main.cpp
@@ -6,7 +6,7 @@
 
 #include <tchar.h>
 #include <windows.h>
-
+#include <math.h>
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 
@@ -75,6 +75,10 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     return messages.wParam;
 }
 
+bool isPlaying;
+float angle;
+int color, figure;
+HWND checks[3];
 
 /*  This function is called by the Windows function DispatchMessage()  */
 
@@ -84,26 +88,107 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     {
     case WM_CREATE:
     {
-        HDC hdc = GetDC(hwnd);
-        HWND radioButton_1 = CreateWindowEx(0, "Button", "Rectangle", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 4, 10, 100, 20, hwnd, NULL, NULL, NULL);
-        HWND radioButton_2 = CreateWindowEx(0, "Button", "Ellipse", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 4, 30, 100, 20, hwnd, NULL, NULL, NULL);
-        HWND radioButton_3 = CreateWindowEx(0, "Button", "Pie", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 4, 50, 100, 20, hwnd, NULL, NULL, NULL);
-        HWND checkBox_1 = CreateWindowEx(0,"Button","Red",WS_VISIBLE | WS_CHILD | BS_CHECKBOX,4,70,100,20,hwnd,NULL,NULL,NULL);
-        HWND checkBox_2 = CreateWindowEx(0,"Button","Green",WS_VISIBLE | WS_CHILD | BS_CHECKBOX,4,90,100,20,hwnd,NULL,NULL,NULL);
-        HWND checkBox_3 = CreateWindowEx(0,"Button","Blue",WS_VISIBLE | WS_CHILD | BS_CHECKBOX,4,110,100,20,hwnd,NULL,NULL,NULL);
-        HWND pushButton_1 = CreateWindow( "Button", "Play", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,4,130,50,20,hwnd,NULL,NULL,NULL);
-        HWND pushButton_2 = CreateWindow( "Button", "Pause", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,54,130,50,20,hwnd,NULL,NULL,NULL);
-
-
+        HWND radioButton_1 = CreateWindowEx(0, "Button", "Rectangle", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 4, 10, 100, 20, hwnd, (HMENU)1, NULL, NULL);
+        HWND radioButton_2 = CreateWindowEx(0, "Button", "Ellipse", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 4, 30, 100, 20, hwnd, (HMENU)2, NULL, NULL);
+        HWND radioButton_3 = CreateWindowEx(0, "Button", "Pie", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 4, 50, 100, 20, hwnd, (HMENU)3, NULL, NULL);
+        HWND checkBox_1 = CreateWindowEx(0,"Button","Red",WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,4,70,100,20,hwnd,(HMENU)4,NULL,NULL);
+        HWND checkBox_2 = CreateWindowEx(0,"Button","Green",WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,4,90,100,20,hwnd,(HMENU)5,NULL,NULL);
+        HWND checkBox_3 = CreateWindowEx(0,"Button","Blue",WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,4,110,100,20,hwnd,(HMENU)6,NULL,NULL);
+        HWND pushButton_1 = CreateWindow( "Button", "Play", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,4,130,50,20,hwnd,(HMENU)7,NULL,NULL);
+        HWND pushButton_2 = CreateWindow( "Button", "Pause", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,54,130,50,20,hwnd,(HMENU)8,NULL,NULL);
+        checks[0] = checkBox_1; checks[1] = checkBox_2; checks[2] = checkBox_3;
         break;
     }
     case WM_COMMAND:
     {
+        switch (wParam)
+        {
+        case 1:
+            figure = 0;
+            break;
+        case 2:
+            figure = 1;
+            break;
+        case 3:
+            figure = 2;
+            break;
+        case 4:
+            color = 0;
+            SendMessage(checks[1], BM_SETCHECK,BST_UNCHECKED,0 );
+            SendMessage(checks[2], BM_SETCHECK,BST_UNCHECKED,0 );
 
+//            CheckDlgButton(checks[0], 4,  BST_CHECKED);
+//            EnableWindow(checks[1], false);
+//            EnableWindow(checks[2], false);
+            break;
+        case 5:
+            SendMessage(checks[0], BM_SETCHECK,BST_UNCHECKED,0 );
+            SendMessage(checks[2], BM_SETCHECK,BST_UNCHECKED,0 );
+//            EnableWindow(checks[0], false);
+//            EnableWindow(checks[2], false);
+            color = 1;
+            break;
+        case 6:
+            SendMessage(checks[0], BM_SETCHECK,BST_UNCHECKED,0 );
+            SendMessage(checks[1], BM_SETCHECK,BST_UNCHECKED,0 );
+//            EnableWindow(checks[0], false);
+//            EnableWindow(checks[1], false);
+            color = 2;
+            break;
+        case 7:
+            SetTimer(hwnd, 7, 100, NULL);
+            break;
+        case 8:
+            KillTimer(hwnd, 7);
+            break;
+        default:
+            break;
+        }
+        InvalidateRect(hwnd, NULL, FALSE);
         break;
     }
-    case BN_CLICKED:
+    case WM_PAINT:
     {
+        PAINTSTRUCT ps;
+        RECT rc;
+        GetClientRect(hwnd, &rc);
+        HDC hdc = BeginPaint (hwnd, &ps) ;
+        FillRect(hdc, &rc, CreateSolidBrush(RGB(255,255,255)));
+
+        switch (color)
+        {
+        case 0:
+            SelectObject(hdc, CreateSolidBrush(RGB(255,0,0)));
+            break;
+        case 1:
+            SelectObject(hdc, CreateSolidBrush(RGB(0,255,0)));
+            break;
+        case 2:
+            SelectObject(hdc, CreateSolidBrush(RGB(0,0,255)));
+            break;
+        }
+        int x= 300 + cos(angle)*100;
+        int y= 150 + sin(angle)*100;
+        switch (figure)
+        {
+        case 0:
+            Rectangle(hdc, x-10, y-10, x+10, y+10);
+            break;
+        case 1:
+            Ellipse(hdc, x-10, y-10, x+10, y+10);
+            break;
+        case 2:
+            Pie(hdc, x-10, y-10, x+10, y+10, x-5,y-10,x+10,y+5);
+            break;
+        }
+        EndPaint (hwnd, &ps) ;
+        break;
+    }
+
+    case WM_TIMER:
+    {
+        angle+=3.14*5/180;
+        InvalidateRect(hwnd, NULL, FALSE);
         break;
     }
     case WM_DESTROY:
